89f8ddf1a429273cfc50098fd5fbc6234c7a38e7
Thomas <letter2tommy@yahoo.com>
2023-03-23 17:06:42 -0400

    Creating API view and configure view URL

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -11,11 +11,10 @@ class HelloApiView(APIView):
 
     def get(self, request, format=None):
         """Returns a list of APIView features"""
-
         an_apiview = [
-            'Uses HTTP methods as functions (get, post, patch, put, delete)',
+            'Uses HTTP methods as function (get, post, patch, put, delete)',
             'Is similar to a traditional Django View',
-            'Gives you the most control over your logic',
+            'Gives you the most control over you application logic',
             'Is mapped manually to URLs',
         ]
 
@@ -27,10 +26,10 @@ class HelloApiView(APIView):
 
         if serializer.is_valid():
             name = serializer.validated_data.get('name')
-            message = f'hello {name}'
+            message = f'Hello {name}'
             return Response({'message': message})
         else:
-            return Reponse(
+            return Response(
                 serializer.errors,
                 status=status.HTTP_400_BAD_REQUEST
             )
